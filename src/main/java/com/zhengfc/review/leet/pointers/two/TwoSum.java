a30e5e905b2e1db3e50b7bbc456ab53405003b8f
fczheng <zhengfc323@gmail.com>
2020-09-14 10:02:11 +0800

    feat(setSolution): Pair with Target Sum (easy)

--- src/main/java/com/zhengfc/review/leet/pointers/two/TwoSum.java
+++ src/main/java/com/zhengfc/review/leet/pointers/two/TwoSum.java
@@ -1,11 +1,25 @@
 package com.zhengfc.review.leet.pointers.two;
 
+import java.util.HashSet;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class TwoSum {
 	public int[] targetSum(int[] arr, int target) {
-		return forceSolution(arr, target);
+		return setSolution(arr, target);
+//		return forceSolution(arr, target);
+	}
+
+	int[] setSolution(int[] arr, int target) {
+		var storeSet = new HashSet<Integer>();
+		for (int i = 0; i < arr.length; i++) {
+			storeSet.add(arr[i]);
+			if (storeSet.contains(target - arr[i])) {
+				return new int[] { arr[i], target - arr[i] };
+			}
+		}
+		return null;
 	}
 
 	int[] forceSolution(int[] arr, int target) {
