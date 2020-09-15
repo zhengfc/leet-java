4e2c645f2f9659224d2ce5d8d635dc664b9b74c4
fczheng <zhengfc323@gmail.com>
2020-09-15 10:37:39 +0800

    feat(fast slow): middle linked list

--- src/main/java/com/zhengfc/review/leet/pointers/fastslow/MiddleLinkedList.java
+++ src/main/java/com/zhengfc/review/leet/pointers/fastslow/MiddleLinkedList.java
@@ -6,7 +6,20 @@ import com.zhengfc.review.leet.ListNode;
 
 public class MiddleLinkedList {
 	public ListNode middle(ListNode head) {
-		return forceSolution(head);
+		return fastSlowSolution(head);
+	}
+
+	ListNode fastSlowSolution(ListNode head) {
+		if (head == null)
+			return head;
+		ListNode slow = head;
+		ListNode fast = head;
+		while (fast != null && fast.getNext() != null) {
+			slow = slow.getNext();
+			fast = fast.getNext().getNext();
+		}
+		return slow;
+
 	}
 
 	ListNode forceSolution(ListNode head) {
