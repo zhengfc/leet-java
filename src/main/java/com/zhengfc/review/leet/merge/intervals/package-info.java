02ecfaa35d62ca1ea672b9fa69737cd7bfd96494
fczheng <zhengfc323@gmail.com>
2020-09-15 10:44:51 +0800

    chore: merge intervals package info

--- src/main/java/com/zhengfc/review/leet/merge/intervals/package-info.java
+++ src/main/java/com/zhengfc/review/leet/merge/intervals/package-info.java
@@ -1,1 +1,8 @@
+/**
+ * Merge Intervals (medium) <br>
+ * Insert Interval (medium) <br>
+ * Intervals Intersection (medium) <br>
+ * Conflicting Appointments (medium) <br>
+ */
+
 package com.zhengfc.review.leet.merge.intervals;
