7348057451fa21a33965240e595ed7da25e0dad3
fczheng <zhengfc323@gmail.com>
2020-09-15 14:49:19 +0800

    feat: intervals merge

--- src/main/java/com/zhengfc/review/leet/merge/intervals/MergeIntervals.java
+++ src/main/java/com/zhengfc/review/leet/merge/intervals/MergeIntervals.java
@@ -1,7 +1,34 @@
 package com.zhengfc.review.leet.merge.intervals;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class MergeIntervals {
-	int[][] merge(int[][] arr) {
-		return null;
+	public int[][] merge(int[][] arr) {
+		return intervalMerge(arr);
+	}
+
+	int[][] intervalMerge(int[][] arr) {
+		if (arr == null || arr.length == 1)
+			return arr;
+		Arrays.sort(arr, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				return o1[0] - o2[0];
+			}
+		});
+		var result = new ArrayList<int[]>();
+		int[] newInterval = arr[0];
+		result.add(newInterval);
+		for (int[] interval : arr) {
+			if (newInterval[1] >= interval[0]) {
+				newInterval[1] = Math.max(newInterval[1], interval[1]);
+			} else {
+				newInterval = interval;
+				result.add(newInterval);
+			}
+		}
+		return result.toArray(new int[result.size()][]);
 	}
 }
--- src/test/java/com/zhengfc/review/leet/merge/intervals/MergeIntervalsTest.java
+++ src/test/java/com/zhengfc/review/leet/merge/intervals/MergeIntervalsTest.java
@@ -6,6 +6,9 @@ import java.util.Arrays;
 
 import org.junit.Test;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class MergeIntervalsTest {
 	MergeIntervals mi = new MergeIntervals();
 
@@ -14,11 +17,13 @@ public class MergeIntervalsTest {
 		int[][] arr1 = { { 1, 3 }, { 2, 6 }, { 8, 10 }, { 15, 18 } };
 		int[][] exp1 = { { 1, 6 }, { 8, 10 }, { 15, 18 } };
 		int[][] act1 = mi.merge(arr1);
+		log.info("act1: {}", Arrays.deepToString(act1));
 		assertTrue(Arrays.deepEquals(exp1, act1));
 
 		int[][] arr2 = { { 1, 4 }, { 4, 5 } };
 		int[][] exp2 = { { 1, 5 } };
 		int[][] act2 = mi.merge(arr2);
+		log.info("act2: {}", Arrays.deepToString(act2));
 		assertTrue(Arrays.deepEquals(exp2, act2));
 	}
 }
