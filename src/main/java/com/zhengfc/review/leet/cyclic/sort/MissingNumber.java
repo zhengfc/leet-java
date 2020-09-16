c3ff00a7f21b5f9c9ad80e0cd019bc4e1030ea08
fczheng <zhengfc323@gmail.com>
2020-09-16 14:18:17 +0800

    feat(missing number): sum solution

--- src/main/java/com/zhengfc/review/leet/cyclic/sort/MissingNumber.java
+++ src/main/java/com/zhengfc/review/leet/cyclic/sort/MissingNumber.java
@@ -4,6 +4,20 @@ import java.util.HashSet;
 
 public class MissingNumber {
 	public int missingOne(int[] arr) {
+//		return setSolution(arr);
+		return sumSolution(arr);
+	}
+
+	int sumSolution(int[] arr) {
+		int n = arr.length;
+		int sum = 0;
+		for (int i = 0; i < n; i++) {
+			sum += arr[i];
+		}
+		return n * (n + 1) / 2 - sum;
+	}
+
+	int setSolution(int[] arr) {
 		var nset = new HashSet<Integer>();
 		for (int i = 0; i <= arr.length; i++)
 			nset.add(i);
