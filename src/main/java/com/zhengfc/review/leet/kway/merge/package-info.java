0138f2f9d59da779901b476fdcaf1a939eadad2a
fczheng <zhengfc323@gmail.com>
2020-09-18 17:21:37 +0800

    chore: k way merge package info

--- src/main/java/com/zhengfc/review/leet/kway/merge/package-info.java
+++ src/main/java/com/zhengfc/review/leet/kway/merge/package-info.java
@@ -1,1 +1,8 @@
+/**
+ * Merge K Sorted Lists (medium) <br>
+ * Kth Smallest Number in M Sorted Lists (Medium) <br>
+ * Kth Smallest Number in a Sorted Matrix (Hard) <br>
+ * Smallest Number Range (Hard) <br>
+ */
+
 package com.zhengfc.review.leet.kway.merge;
