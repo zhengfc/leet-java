eed5f93fa378b881965ed96261637d4273ce9685
fczheng <zhengfc323@gmail.com>
2020-09-24 16:10:02 +0800

    chore(modified bs): package info

--- src/main/java/com/zhengfc/review/leet/binary/search/package-info.java
+++ src/main/java/com/zhengfc/review/leet/binary/search/package-info.java
@@ -1,1 +1,11 @@
+/**
+ * Order-agnostic Binary Search (easy) <br>
+ * Bitonic Array Maximum (easy) <br>
+ * Ceiling of a Number (medium) <br>
+ * Next Letter (medium) <br>
+ * Number Range (medium) <br>
+ * Search in a Sorted Infinite Array (medium) <br>
+ * Minimum Difference Element (medium) <br>
+ */
+
 package com.zhengfc.review.leet.binary.search;
